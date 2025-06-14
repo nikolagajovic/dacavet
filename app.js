1e41febd2080d758bd1666ae2ab308595e4a7980
nikolagajovic <iamnikolagajovic@gmail.com>
2025-06-14 13:14:43 +0200

    add animations and google map

--- app.js
+++ app.js
@@ -12,3 +12,56 @@ function toggleMenu() {
         menuIcon.classList.add("fa-bars");
     }
 }
+
+window.onscroll = function () {
+    const btn = document.getElementById("scrollTopBtn");
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        btn.style.display = "flex";
+    } else {
+        btn.style.display = "none";
+    }
+
+    var navbar = document.getElementById("navbar");
+    if (window.pageYOffset > 50) {
+        navbar.classList.add("transparent-navbar");
+    } else {
+        navbar.classList.remove("transparent-navbar");
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// animations
+
+const allObservedElements = [
+    ...document.querySelectorAll('.service-box'),
+    document.querySelector('.map-container')
+];
+
+
+document.querySelectorAll('.service-box').forEach((box, i) => {
+    box.classList.add(`delay-${i + 1}`);
+});
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+
+allObservedElements.forEach(el => {
+    observer.observe(el);
+});
+
